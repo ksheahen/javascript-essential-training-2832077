d8d5525955fb8b9ac7f4d2f40e655a7461a2bb1b
Kathryn Sheahen <112595660+ksheahen@users.noreply.github.com>
2025-02-07 10:25:36 -0600

    practice 1

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,35 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const backpack = {
+  color: "grey",
+  size: "small",
+  strapLength: {
+    left: 35,
+    right: 30,
+  },
+  pockets: "2",
+};
+
+const drink = {
+  type: "iced",
+  flavor: "matcha latte",
+  size: "large",
+};
+
+const computer = {
+  cpu: "intel",
+  gpu: "nvidia 4080",
+  psu: "corsair 600W",
+  ram: 32,
+  mobo: "msi whatever",
+  monitor: {
+    width: 27,
+    name: "dell whtever",
+  },
+};
+
+console.log("the backpack: ", backpack);
+console.log("drink: ", drink.type, drink.flavor);
+console.log("computer monitor: ", computer.monitor.name);
